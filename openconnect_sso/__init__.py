97ec9b0ef5183c5ac0edc5bf37d36dde12422d12
Khoa Hoang <admin@khoahoang.com>
2021-09-23 18:57:12 -0700

    maximus hax

--- openconnect_sso/__init__.py
+++ openconnect_sso/__init__.py
@@ -1,11 +1,11 @@
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    # Python < 3.8
-    import importlib_metadata
+# try:
+#     import importlib.metadata as importlib_metadata
+# except ModuleNotFoundError:
+#     # Python < 3.8
+#     import importlib_metadata
 
 
-_metadata = importlib_metadata.metadata("openconnect-sso")
+# _metadata = importlib_metadata.metadata("openconnect-sso")
 
-__version__ = _metadata["Version"]
-__description__ = _metadata["Summary"]
+__version__ = "1"#_metadata["Version"]
+__description__ = "1" #_metadata["Summary"]
